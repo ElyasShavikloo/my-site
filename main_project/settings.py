a8982e5c5e91376120f48fd3dee713b92984cc3a
Elyas Shavikloo <shaviklooe@gmail.com>
2023-10-25 16:40:58 +0330

    "مرحله دوم: ابتدا اپ هوم رو در فایل ستینگ پروژه معرفی کردم و بعد از ابجاد تمپلیت و انتقال تمپلیت ایندکس؛ در تمپلیت اصلی پروژه بیس رو ساختم و قسمت بندی کردم و در نهایت در تمپلیت ایندکس از بیس ارث بری کردم و در آخر ویو و یوآرال رو ست کردم و به یو آر ال اصلی معرفی کردم"

--- main_project/settings.py
+++ main_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my apps
+    'home.apps.HomeConfig',
+
 
 
 
--- main_project/urls.py
+++ main_project/urls.py
@@ -21,4 +21,5 @@ from main_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
